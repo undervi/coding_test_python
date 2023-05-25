89dbbf30ae668930cbdb449ce5a3c85416c764e3
undervi <95211722+undervi@users.noreply.github.com>
2023-05-25 20:39:37 +0900

    [Bronze V] Title: 영수증, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/25304. 영수증/영수증.py
@@ -0,0 +1,12 @@
+sum = int(input())
+n = int(input())
+result = 0
+
+for _ in range(n):
+    a, b = map(int, input().split())
+    result += (a * b)
+
+if sum == result:
+    print("Yes")
+else:
+    print("No")
