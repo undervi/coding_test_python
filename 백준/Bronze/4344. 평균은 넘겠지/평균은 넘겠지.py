91a684f95a331503cd504b92898a23d7d5864f39
undervi <95211722+undervi@users.noreply.github.com>
2023-05-25 22:21:07 +0900

    [Bronze I] Title: 평균은 넘겠지, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.py
@@ -0,0 +1,16 @@
+import sys
+n = int(sys.stdin.readline().rstrip())
+
+for _ in range(n):
+    array = list(map(int, sys.stdin.readline().split()))
+    count = array[0]
+    array.remove(array[0])
+    av = sum(array) / count
+    av_cnt = 0
+    
+    for x in array:
+        if x > av:
+            av_cnt += 1
+          
+    result = round(av_cnt/count * 100, 3)
+    print(f"{result:.3f}%")
