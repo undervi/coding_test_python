a994445a99dafb16fa401133f954797060e9b19c
undervi <95211722+undervi@users.noreply.github.com>
2023-06-25 18:09:15 +0900

    [Bronze V] Title: 코드마스터 2023, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.py
@@ -0,0 +1,10 @@
+battle = input()
+
+if battle == "SONGDO":
+  print("HIGHSCHOOL")
+elif battle == "CODE":
+  print("MASTER")
+elif battle == "2023":
+  print("0611")
+elif battle == "ALGORITHM":
+  print("CONTEST")
