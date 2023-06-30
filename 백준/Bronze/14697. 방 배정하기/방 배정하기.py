a61764dfb52a65ea1487ab6a74d6415f3bbbb214
undervi <95211722+undervi@users.noreply.github.com>
2023-07-01 01:57:40 +0900

    [Bronze II] Title: 방 배정하기, Time: 44 ms, Memory: 33376 KB -BaekjoonHub

---
+++ 백준/Bronze/14697. 방 배정하기/방 배정하기.py
@@ -0,0 +1,19 @@
+import math
+a, b, c, number = map(int, input().split())
+
+if number % a == 0 | number % b == 0 | number % c == 0:
+    print(1)
+else:
+    result = 0
+    for i in range(math.ceil(number/a)):
+        for j in range(math.ceil(number/b)):
+            for k in range(math.ceil(number/c)):
+                if a*i + b*j + c*k == number:
+                    result = 1
+                    break
+            if result == 1:
+                break
+        if result == 1:
+            break
+          
+    print(result)
