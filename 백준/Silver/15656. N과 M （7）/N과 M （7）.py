8b7a05b6cb5c220437e6cd665adf7d1e0fb1dc5f
undervi <95211722+undervi@users.noreply.github.com>
2023-07-08 03:12:34 +0900

    [Silver III] Title: N과 M (7), Time: 2180 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+
+nums = list(map(int, input().split()))
+nums.sort()
+arr = []
+
+def recur(count):
+    if count == m:  # 기저 조건
+        print(*arr)
+        return
+      
+    for num in nums:
+        arr.append(num)
+        recur(count+1)
+        arr.pop()
+
+recur(0)
