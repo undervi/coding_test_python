fb359d3925353f03c6ec3380f5cbecc82a7fb0c5
undervi <95211722+undervi@users.noreply.github.com>
2023-07-26 00:55:54 +0900

    [Silver II] Title: 연속합, Time: 100 ms, Memory: 38964 KB -BaekjoonHub

---
+++ 백준/Silver/1912. 연속합/연속합.py
@@ -0,0 +1,8 @@
+n = int(input())
+arr = list(map(int, input().split()))
+prefix = [0 for _ in range(n+1)]
+
+for i in range(n):
+    prefix[i+1] = max(prefix[i] + arr[i], arr[i])
+
+print(max(prefix[1:]))
