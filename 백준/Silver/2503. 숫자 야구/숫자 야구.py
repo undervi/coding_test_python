31a5e8f13730ab861ebe30df7c9b7a86fabb2bc6
undervi <95211722+undervi@users.noreply.github.com>
2023-07-26 08:39:36 +0900

    [Silver III] Title: 숫자 야구, Time: 48 ms, Memory: 31556 KB -BaekjoonHub

--- 백준/Silver/2503. 숫자 야구/숫자 야구.py
+++ 백준/Silver/2503. 숫자 야구/숫자 야구.py
@@ -1,25 +1,73 @@
-from itertools import permutations
+import sys
+sys.setrecursionlimit(10**9)
+input = sys.stdin.readline
+
+def checker(number, hint_idx):
+    # check number
+    strike = 0
+    ball = 0
+    A = number // 100               # 100의 자리
+    B = (number - A*100) // 10      # 10의 자리
+    C = number % 10                 # 1의 자리
+
+    # 힌트
+    _number = hint[hint_idx][0]
+    _strike = hint[hint_idx][1]
+    _ball = hint[hint_idx][2]
+    _A = _number // 100             # 100의 자리
+    _B = (_number - _A*100) // 10   # 10의 자리
+    _C = _number % 10               # 1의 자리
+
+    # ball
+    if A == _B or A == _C:
+        ball += 1
+    if B == _A or B == _C:
+        ball += 1
+    if C == _A or C == _B:
+        ball += 1
+
+    # strike
+    if A == _A:
+        strike += 1
+    if B == _B:
+        strike += 1
+    if C == _C:
+        strike += 1
+
+    # ball_cnt와 strike_cnt가 hint와 일치하는지 여부 리턴
+    return ball == _ball and strike == _strike
+
+def recur(hint_idx, number):
+    global answer 
+
+    # 기저 조건
+    if number == 1000:
+        return
+
+    # 조건에 맞는 숫자만 진행
+    num_arr = list(map(int, str(number)))
+    if len(set(num_arr)) == 3 and 0 not in num_arr: 
     
+        # 힌트에 모두 통과했다면 answer에 1 더해주고 다음 숫자 진행
+        if hint_idx == n:
+            answer += 1
+            recur(0, number+1)
+            return
+        
+        # 힌트에 통과했다면 힌트 번호 올리기
+        if checker(number, hint_idx):
+            recur(hint_idx+1, number)
+        # 만약에 힌트에 통과하지 않았다면 -> 다음 숫자 진행
+        else:
+            recur(0, number+1)
+
+    # 조건에 맞지 않다면 다음 숫자로 넘기기
+    else:
+        recur(0, number+1)
+
 n = int(input())
-result = list(permutations(['1', '2', '3', '4', '5', '6', '7', '8', '9'], 3))
-del_list = []
+hint = [list(map(int, input().split())) for _ in range(n)]
+answer = 0
 
-for _ in range(n):
-    number, st, ball = map(int, input().split())
-    number = str(number)
-    for rs in result:
-        rs_num = rs[0]+rs[1]+rs[2]
-
-        # 볼 제거
-        a, b, c = 1 if number[0] in rs_num else 0, 1 if number[1] in rs_num else 0, 1 if number[2] in rs_num else 0
-        if ball + st != a + b + c:
-            del_list.append(rs)
-          
-        # 스트라이크 제거
-        d, e, f = 1 if rs[0] == number[0] else 0, 1 if rs[1] == number[1] else 0, 1 if rs[2] == number[2] else 0
-        if st != d + e + f:
-            del_list.append(rs)
-      
-
-result = [x for x in result if x not in del_list]
-print(len(result))
+recur(0, 100)
+print(answer)
