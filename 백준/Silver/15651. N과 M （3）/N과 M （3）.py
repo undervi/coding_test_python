4e0e0172e078255114ce12026dd1a6934ace0d20
undervi <95211722+undervi@users.noreply.github.com>
2023-07-26 06:47:59 +0900

    Update N과 M （3）.py

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.py
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -1,13 +1,15 @@
-import sys
-input = sys.stdin.readline
-
-def recur(cnt, num):
-    if cnt == m:
-        print(num.strip())
+def recur(num):
+    if num == m:
+        print(*arr)
         return
 
     for i in range(1, n+1):
-        recur(cnt+1, num+" "+str(i))
-  
+        arr.append(i)
+        recur(num+1)
+        arr.pop()
+
+# n은 숫자의 범위 (1~n), m은 반복할 횟수
 n, m = map(int, input().split())
-recur(0, "")
+arr = []
+
+recur(0)
