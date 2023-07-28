2eb7a139b245829ece08252c1f5426a8d9ce6ed1
undervi <95211722+undervi@users.noreply.github.com>
2023-07-29 04:52:22 +0900

    [Gold V] Title: 평범한 배낭, Time: 2000 ms, Memory: 156756 KB -BaekjoonHub

---
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -0,0 +1,22 @@
+import sys
+sys.setrecursionlimit(99999999)
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+items = [list(map(int, input().split())) for _ in range(n)]
+dp = [[-1 for _ in range(100_001)] for _ in range(n)]
+
+def recur(idx, weight):
+    if weight > k:
+        return -999999
+    if idx == n:
+        return 0
+    if dp[idx][weight] != -1:
+        return dp[idx][weight]
+
+    # 물건을 넣은 경우, 넣지 않은 경우
+    dp[idx][weight] = max(recur(idx+1, weight+items[idx][0]) + items[idx][1], recur(idx+1, weight)) 
+    return dp[idx][weight]
+
+ans = recur(0,0)
+print(ans)
